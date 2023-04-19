35d01ccc3b2e1e3846da237a6dbcbd9b7097f623
Kohaikaa <cherry.designwork@gmail.com>
2023-04-19 23:36:30 +0300

    Homework 8 is done

--- homework8.sql
+++ homework8.sql
@@ -1,32 +1,32 @@
 use vk;
 -- 1. Вывести все файлы видео с расширениями .avi и .mp4 
-select
-	m.filename
-from
-	media m
-where 
+	select
+		m.filename
+	from
+		media m
+	where 
 	substring_index(m.filename, '.', -1) = 'avi'
-union 
-select 
-	m.filename 
-from
-	media m
-where m.filename like '%.mp4'
+union
+	select
+		m.filename
+	from
+		media m
+	where m.filename like '%.mp4'
 -- 2. Пусть задан некоторый пользователь. Из всех друзей этого пользователя найдите человека, 
 -- который больше всех общался с выбранным пользователем (написал ему сообщений).
-select 
+select
 	concat(u.firstname, " ", u.lastname) as FullName,
 	count(m2.from_user_id) as MessagesCount
 from
 	users u
-join
-	friend_requests fr 
-on
+	join
+	friend_requests fr
+	on
 	fr.target_user_id = 1 or
-	fr.initiator_user_id = 1
-join
+		fr.initiator_user_id = 1
+	join
 	messages m2
-on
+	on
 	m2.to_user_id = 1
 where fr.status = 'approved'
 group by u.id
@@ -35,23 +35,23 @@ limit 1;
 select
 	concat(u.firstname, " ", u.lastname) as FullName,
 	count(*) as Likes
-from 
+from
 	likes l2
-inner join
-	profiles p2 
-on
+	inner join
+	profiles p2
+	on
 	p2.user_id = l2.user_id
-inner join
-	users u 
-on
-	u.id = p2.user_id 
+	inner join
+	users u
+	on
+	u.id = p2.user_id
 where
 	((YEAR(CURRENT_DATE) - YEAR(p2.birthday))
 	- (DATE_FORMAT(CURRENT_DATE, '%m%d')
 	< DATE_FORMAT(p2.birthday, '%m%d'))
-    ) < 11
+) < 11
 group by l2.user_id;
-    
+
 -- 4. Определить кто больше поставил лайков (всего): мужчины или женщины.
 select
 	CASE
@@ -62,11 +62,11 @@ select
     ELSE 'Не определен'
 END AS 'Sex',
 	count(*) as "Likes"
-from 
+from
 	likes l
-join
+	join
 	profiles p
-on
+	on
 	p.user_id = l.user_id
 group by p.gender;
 
@@ -77,9 +77,9 @@ select
 	count(c.id) as "Количество групп"
 from
 	communities c
-inner join
+	inner join
 	users_communities uc
-on
+	on
 	uc.community_id = c.id
 group by uc.user_id;
 
@@ -89,22 +89,23 @@ select
 	count(uc.user_id) as "Количество пользователей"
 from
 	users_communities uc
-inner join
+	inner join
 	communities c
-on
+	on
 	c.id = uc.community_id
-group by uc.community_id ; 
+group by uc.community_id
+;
 
 -- 7. Найти 3 пользователей с наибольшим количеством лайков
-select 
+select
 	u.id,
 	u.firstname,
-	count(l.user_id)  
+	count(l.user_id)
 from
 	users u
-inner join
+	inner join
 	likes l
-on
+	on
 l.user_id = u.id
 group by l.user_id
 limit 3;
