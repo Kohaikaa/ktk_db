5562329af313c59690821978a7fefe5c138564f9
Kohaikaa <cherry.designwork@gmail.com>
2023-04-27 08:31:05 +0300

    Some corrections

--- ControlPoint2.sql
+++ ControlPoint2.sql
@@ -1,71 +1,70 @@
 -- Простые запросы
 SELECT
-	c.FirstName,
-	c.LastName,
-	c.Email
+ c.FirstName,
+ c.LastName,
+ c.Email
 FROM
-	customers c
+ customers c
 where c.Company is not null;
 
-select
+select 
 	a.Title
-from
+from 
 	albums a
 where a.Title like "A%"
 
 
 -- Запросы с агрегациями
 SELECT
-	COUNT(c.LastName) AS NameCountStartsWithB
+ COUNT(c.LastName) AS NameCountStartsWithB
 FROM
-	customers c
+ customers c
 WHERE
  LastName LIKE 'B%'
-
+ 
 select
 	any_value(concat(c.FirstName, " ", c.LastName)) as Fullname,
-	count(*) as 'Customers from USA',
 	c.Country
 from
 	customers c
 where c.Country = 'USA'
-group by c.FirstName;
-
+group by Fullname;
+ 
 -- Сложные запросы с объединением 2-3 таблиц с помощью Join 2-3
-SELECT
-	c.LastName,
+select
 	c.FirstName,
-	i.InvoiceId,
-	i.CustomerId,
-	i.InvoiceDate,
-	i.Total
+ 	c.LastName,
+ 	i.InvoiceId,
+ 	i.CustomerId,
+ 	i.InvoiceDate,
+ 	i.Total
 FROM
 	invoices AS i
-	INNER JOIN
+INNER JOIN
 	customers AS c
-	ON
- i.CustomerId = c.CustomerId
+ON
+	i.CustomerId = c.CustomerId
 ORDER BY
- c.LastName
-
+	c.LastName
+ 
 SELECT
-	e.FirstName,
-	e.LastName,
-	e.EmployeeId,
-	c.FirstName,
-	c.LastName,
-	c.SupportRepId,
-	i.CustomerId,
-	i.Total
+ e.FirstName,
+ e.LastName,
+ e.EmployeeId,
+ c.FirstName,
+ c.LastName,
+ c.SupportRepId,
+ i.CustomerId,
+ i.Total
 FROM
-	invoices AS i
-	INNER JOIN
-	customers AS c
-	ON
+ invoices AS i
+INNER JOIN
+ customers AS c
+ON
  i.CustomerId = c.CustomerId
-	INNER JOIN
-	employees AS e
-	ON
+INNER JOIN
+ employees AS e
+ON
  c.SupportRepId = e.EmployeeId
 ORDER BY
  i.Total DESC
@@ -73,19 +72,14 @@ LIMIT 10
 
 -- Сложные запросы с объединением 2-3 таблиц подзапросами 2-3
 select
-	(select a2
-.Name from artists a2 where a2.ArtistId = a.ArtistId) as "Artist Name",
+	(select a2.Name from artists a2 where a2.ArtistId = a.ArtistId) as "Artist Name",
 	a.Title as "Album title"
 from
 	albums a;
-
-select
+	
+select 
 	t.Name,
-	(select a.Title
-	from albums a
-	where a.AlbumId = t.AlbumId ) as "Album title",
-	(select gen.Name
-	from genres gen
-	where gen.GenreId = t.GenreId) as "Genre"
-from
+	(select a.Title  from albums a where a.AlbumId = t.AlbumId ) as "Album title",
+	(select gen.Name  from genres gen where gen.GenreId = t.GenreId) as "Genre"
+from 
 	tracks t;
