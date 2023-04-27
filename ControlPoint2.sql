17db1dffab1322cd6f8bad8a912a9b7d970f1bf6
Kohaikaa <cherry.designwork@gmail.com>
2023-04-27 08:50:50 +0300

    Added comments

--- ControlPoint2.sql
+++ ControlPoint2.sql
@@ -1,4 +1,6 @@
 -- Простые запросы
+
+-- Клиенты, работающие в компании.
 SELECT
  c.FirstName,
  c.LastName,
@@ -7,6 +9,7 @@ FROM
  customers c
 where c.Company is not null;
 
+-- Названия альбомов начинающиеся на «А»
 select 
 	a.Title
 from 
@@ -15,6 +18,7 @@ where a.Title like "A%"
 
 
 -- Запросы с агрегациями
+-- Клиенты из США
 SELECT
  COUNT(c.LastName) AS NameCountStartsWithB
 FROM
@@ -31,22 +35,28 @@ where c.Country = 'USA'
 group by Fullname;
  
 -- Сложные запросы с объединением 2-3 таблиц с помощью Join 2-3
+-- Клиенты, проживающие в Германии с выплатой меньше 10
 select
 	c.FirstName,
  	c.LastName,
  	i.InvoiceId,
  	i.CustomerId,
+ 	c.Country,
  	i.InvoiceDate,
  	i.Total
 FROM
 	invoices AS i
+
 INNER JOIN
 	customers AS c
 ON
 	i.CustomerId = c.CustomerId
+where (i.Total <= 10) and c.Country = 'Germany'
 ORDER BY
 	c.LastName
- 
+
+	
+-- Клиенты, к которым привязан личной сотрудник.
 SELECT
  e.FirstName,
  e.LastName,
@@ -71,12 +81,14 @@ ORDER BY
 LIMIT 10
 
 -- Сложные запросы с объединением 2-3 таблиц подзапросами 2-3
+-- Имя артиста и название альбома
 select
 	(select a2.Name from artists a2 where a2.ArtistId = a.ArtistId) as "Artist Name",
 	a.Title as "Album title"
 from
 	albums a;
-	
+
+-- Треки, их жанр и в какому альбоме они находятся
 select 
 	t.Name,
 	(select a.Title  from albums a where a.AlbumId = t.AlbumId ) as "Album title",
